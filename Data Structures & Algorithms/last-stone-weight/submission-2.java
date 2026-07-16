134bc407337f54c10adeece8c509634f8c98450d
Sada Kurapati <sadakurapati@gmail.com>
2026-07-16 00:11:04 -0700

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        if(stones == null || stones.length == 0) return 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> Integer.compare(b, a)
+        );
+        for (int stone: stones) {
+            pq.offer(stone);
+        }
+        while(pq.size() > 1) {
+            int first = pq.poll();
+            int second = pq.poll();
+            // Smash and put remaining in the pq.
+            pq.offer(Math.abs(first - second));
+        }
+        return pq.peek();
+    }
+}
